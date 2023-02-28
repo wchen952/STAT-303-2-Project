b5822ff977c78138fe4f7a0daf0a9ea6f85f313f
MarceloBarillas10 <47263699+MarceloBarillas10@users.noreply.github.com>
2023-02-27 18:02:11 -0600

    testing

--- basketball.py
+++ basketball.py
@@ -1,1 +1,20 @@
-working doc
+# import pandas as pd
+# import numpy as np
+# import statsmodels.formula.api as smf
+# import seaborn as sns
+# import matplotlib.pyplot as plt
+# import statsmodels.api as sm
+
+
+# basketball_abbrev = pd.read_csv("basketball_abbrev.csv")
+# basketball_award_coaches = pd.read_csv("basketball_awards_coaches.csv")
+# basketball_awards_players = pd.read_csv("basketball_awards_players.csv")
+# basketball_coaches = pd.read_csv("basketball_coaches.csv")
+# basketball_draft = pd.read_csv("basketball_draft.csv")
+# basketball_hof = pd.read_csv("basketball_hof.csv")
+# basketball_master = pd.read_csv("basketball_master.csv")
+# basketball_player_allstar = pd.read_csv("basketball_player_allstar.csv")
+# basketball_series_post = pd.read_csv("basketball_series_post.csv")
+# basketball_teams = pd.read_csv("basketball_teams.csv")
+
+print("pff")
